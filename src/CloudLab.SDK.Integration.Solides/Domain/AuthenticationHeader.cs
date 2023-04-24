ade35f3f9395e929116771988af739fb8dae9583
Eric Darruiz <33941948+edarruiz@users.noreply.github.com>
2023-04-24 18:03:12 -0300

    feat: Adicionado AuthenticationHeader.cs
    Adicionada classe de configuração do header padrão de autenticação para todas as requisições.

---
+++ src/CloudLab.SDK.Integration.Solides/Domain/AuthenticationHeader.cs
@@ -0,0 +1,31 @@
+﻿namespace CloudLab.SDK.Integration.Solides.Domain;
+
+/// <summary>
+/// Representa o HEADER de autenticação enviado em todas as requisições de API da Solides.
+/// </summary>
+public sealed record AuthenticationHeader {
+    private string _authorization = string.Empty;
+
+    /// <summary>
+    /// Retorna o token de autorização de acesso à plataforma Solides.
+    /// </summary>
+    /// <remarks>O valor retornado corresponde ao valor do header já formatado
+    /// como <c>Authorization: Token token=VALOR_DO_TOKEN</c>.</remarks>
+    public string Authorization {
+        get => $"Authorization: Token token={_authorization}";
+        set => _authorization = value;
+    }
+
+    /// <summary>
+    /// Retorna o tipo de conteúdo aceito nas requisições, definido como MIME type.
+    /// </summary>
+    /// <remarks>O padrão é <c>application/json</c>.</remarks>
+    public string Accept { get; } = "application/json";
+
+    /// <summary>
+    /// Retorna o tipo de conteúdo utilizado nas requisições, definido como MIME type.
+    /// </summary>
+    /// <remarks>O padrão é <c>application/json</c>.</remarks>
+    public string ContentType { get; init; } = "application/json";
+
+}
