db17b0252962c5cfa1c127e2021e4235e5e92b82
Eric Darruiz <33941948+edarruiz@users.noreply.github.com>
2023-04-25 23:09:33 -0300

    fix: Alterado AuthenticationHeader.cs
    - FIX: Corrigido valor da máscara do Header Authorization
    - REFACTOR: MIME-Type de Accept e ContentType foram movidos para constante, uma vez que ambos possuem o mesmo valor.

--- src/CloudLab.SDK.Integration.Solides/Domain/AuthenticationHeader.cs
+++ src/CloudLab.SDK.Integration.Solides/Domain/AuthenticationHeader.cs
@@ -4,7 +4,15 @@
 /// Representa o HEADER de autenticação enviado em todas as requisições de API da Solides.
 /// </summary>
 public sealed record AuthenticationHeader {
+    /// <summary>
+    /// Field interno de armazenamento do valor da propriedade <see cref="Authorization"/>.
+    /// </summary>
     private string _authorization = string.Empty;
+
+    /// <summary>
+    /// Representa o mime-type padrão para conteúdos no formato Json.
+    /// </summary>
+    private const string MIME_APPLICATION_JSON = "application/json";
 
     /// <summary>
     /// Retorna o token de autorização de acesso à plataforma Solides.
@@ -12,7 +20,7 @@ public sealed record AuthenticationHeader {
     /// <remarks>O valor retornado corresponde ao valor do header já formatado
     /// como <c>Authorization: Token token=VALOR_DO_TOKEN</c>.</remarks>
     public string Authorization {
-        get => $"Authorization: Token token={_authorization}";
+        get => $"Token token={_authorization}";
         set => _authorization = value;
     }
 
@@ -20,12 +28,12 @@ public sealed record AuthenticationHeader {
     /// Retorna o tipo de conteúdo aceito nas requisições, definido como MIME type.
     /// </summary>
     /// <remarks>O padrão é <c>application/json</c>.</remarks>
-    public string Accept { get; } = "application/json";
+    public string Accept { get; } = MIME_APPLICATION_JSON;
 
     /// <summary>
     /// Retorna o tipo de conteúdo utilizado nas requisições, definido como MIME type.
     /// </summary>
     /// <remarks>O padrão é <c>application/json</c>.</remarks>
-    public string ContentType { get; init; } = "application/json";
+    public string ContentType { get; init; } = MIME_APPLICATION_JSON;
 
 }
