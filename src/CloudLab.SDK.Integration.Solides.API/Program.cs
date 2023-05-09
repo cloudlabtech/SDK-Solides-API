a1076a5b3eb05fd6be32a52fc10a3e0947a06838
Eric Darruiz <33941948+edarruiz@users.noreply.github.com>
2023-05-09 01:19:56 -0300

    feat: Adicionados arquivos de configuração por ENV
    Adicionados arquivos de configuração appsettings.json e appsettings.{ENV}.json como opcionais

--- src/CloudLab.SDK.Integration.Solides.API/Program.cs
+++ src/CloudLab.SDK.Integration.Solides.API/Program.cs
@@ -1,24 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
+builder.Configuration
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", optional: true, reloadOnChange: true);
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment()) {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
