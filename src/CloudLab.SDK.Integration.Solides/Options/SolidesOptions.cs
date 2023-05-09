35f37bd9378954fb48a9c264667e78bb22cdc064
Eric Darruiz <33941948+edarruiz@users.noreply.github.com>
2023-05-09 01:23:12 -0300

    feat: Criado SolidesOptions.cs
    Adicionada classe de mapeamento de opções de configuração da API da Solides na aplicação

---
+++ src/CloudLab.SDK.Integration.Solides/Options/SolidesOptions.cs
@@ -0,0 +1,20 @@
+﻿namespace CloudLab.SDK.Integration.Solides.Options;
+
+/// <summary>
+/// Representa as opções de configuração para o consumo da API da Solides.
+/// </summary>
+public sealed class SolidesOptions {
+    /// <summary>
+    /// Representa o nome da seção (Json Property) onde estão localizadas as opções de configuração 
+    /// para o consumo da API da Solides.
+    /// </summary>
+    public const string SectionName = "SolidesAPI";
+
+    #region Properties / Configuration Options
+    /// <summary>
+    /// Retorna ou define o token de autenticação que deve ser enviado no header em todas as requisições.
+    /// </summary>
+    /// <remarks>Saiba como gerar o token de autenticação da API no link <see href="https://gestaoapidocs.solides.com.br/#section/Autenticacao"/></remarks>
+    public required string? AuthenticationToken { get; init; } = null;
+    #endregion
+}
